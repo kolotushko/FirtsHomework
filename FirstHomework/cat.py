6f3172fb4aa1da8f50cdc6e4d788bd6be490b5a2
kolotushko1 <nemo.wabe@gmail.com>
2023-05-20 16:18:57 +0300

    #Pet

---
+++ FirstHomework/cat.py
@@ -0,0 +1,20 @@
+class Pet:
+  def __init__(self, name, hunger, joy, age):
+      self.Name = name
+      self.Hunger = hunger
+      self.Joy = joy
+      self.Age = age
+  def ToString(self):
+      print(f"Name: {self.Name}\n"
+            f"Hunger: {self.Hunger}%\n"
+            f"Joy{self.Joy}%\n"
+            f"Age: {self.Age}\n")
+
+
+
+
+
+
+
+
+
---
+++ FirstHomework/main.py
@@ -0,0 +1,4 @@
+from cat import Pet
+Cat = Pet("Barsik", 70, 100, 3)
+Cat.ToString()
+
